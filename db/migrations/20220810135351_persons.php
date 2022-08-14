c7cf2254a689edcd5ed94cc6a8eb2e086b057391
Arthur Wagenknecht <arthur.wagenknecht@itmapps.com.br>
2022-08-13 23:55:36 -0300

    Updated archives

--- db/migrations/20220810135351_persons.php
+++ db/migrations/20220810135351_persons.php
@@ -7,16 +7,15 @@ final class Persons extends AbstractMigration
 {
     public function change(): void
     {
-        $users = $this->table('person');
-        $users->addColumn('name', 'string', ['limit' => 20])
-              ->addColumn('birth_date', 'string', ['limit' => 40])
-              ->addColumn('cpf', 'string', ['limit' => 40])
-              ->addColumn('sex', 'string', ['limit' => 10])
-              ->addColumn('phone', 'string', ['limit' => 30])
-              ->addColumn('email', 'string', ['limit' => 50])
-              ->addColumn('created', 'datetime')
-              ->addColumn('updated', 'datetime', ['null' => true])
-              ->addIndex(['email'], ['unique' => true])
-              ->create();
+        $person = $this->table('person');
+        
+        $person->addColumn('name', 'string', ['limit' => 20])
+               ->addColumn('birth_date', 'string', ['limit' => 40])
+               ->addColumn('cpf', 'string', ['limit' => 40])
+               ->addColumn('sex', 'string', ['limit' => 10])
+               ->addColumn('phone', 'string', ['limit' => 30])
+               ->addColumn('email', 'string', ['limit' => 50])
+               ->addIndex(['email'], ['unique' => true])
+               ->create();
     }
 }
